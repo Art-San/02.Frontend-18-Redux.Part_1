7e135b6606bbaf37e1bd2f564db0ed873343478e
Aleksander <asava@sibmail.com>
2023-01-20 23:26:38 +0700

    Разработка Redux с нуля. Dispatch

--- src/index.js
+++ src/index.js
@@ -7,13 +7,32 @@ function createStore(initialState) {
   function getState() {
     return state
   }
-  return {getState}
+
+  function dispatch(action) {
+    console.log('action', action)
+    if(action.type === 'task/completed') {
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
+      newArray[elementIndex].completed = true
+      state = newArray
+      console.log('state', state)
+    }
+  }
+
+  return {getState, dispatch}
 }
 const store = createStore([{id: 1, description: 'Task 1', completed: false}])
 
 const App = () => {
-  console.log(store.getState())
-  return <h1>App</h1>
+  console.log('store.getState(', store.getState())
+  
+
+  return (
+    <>
+    <h1>App</h1>
+    <button onClick={() => store.dispatch({ type: 'task/completed', payload: { id: 1}})}>Completed</button>
+    </>
+  )
   
 }
 
