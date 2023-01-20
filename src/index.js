6606cadc2ddbff314acecca92dcdfc7ce054a50b
Aleksander <asava@sibmail.com>
2023-01-20 14:23:34 +0700

    Композиция функций,  piping-трубопровод

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import { compose, pipe } from 'lodash/fp'
 
 const App = () => {
- const arr = [' some', ' new', ' data']
- function formatArray(el) {return el + ' some'}
- 
-  // setTimeout(() => {
-    
-  // }, timeout);
- 
-  return <h1>{arr.map(formatArray)}</h1>
+  const x = 2
+  // const y = x * 2
+  // const z = y * y
+  const double = (number) => number * 2
+  const square = (number) => number * number
+  const half = (number) => number / 2
+  // const output = square(double(x))
+  const mathCalculate = pipe(double,  square, half)
+
+  return <h1>{mathCalculate(x)}</h1>
+  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -18,14 +22,3 @@ root.render(
     <App />
   </React.StrictMode>
 )
-
-
-// function someFn() {
-//   return function() {
-//     return 'apps'
-//   }
-// }
-// function fn(func) {
-// return func()
-
-// }
