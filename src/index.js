797d2fb8fad4e118fcb91a70cecd7471f84878fe
Aleksander <asava@sibmail.com>
2023-01-20 22:27:34 +0700

    Разработка Redux с нуля. Create Store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-const App = () => {
-  const [state, setState] = useState('staring')
-  // setState((prevState) => ({...prevState, name: 'anjela'}))
-  const obj1 = { id: 2, name: 'Some', autor: {name: 'some2'} }
-  // const obj2 = { ...obj1} // true одинаковые ссылки obj1.autor === obj2.autor
-  const obj2 = { ...obj1, autor: { ...obj1.autor } } // false теперь разные ссылки obj1.autor === obj2.autor
-  console.log(obj1.autor === obj2.autor)
-  // console.log(obj2.autor.name)
+function createStore(initialState) {
+  let state = initialState
+  
+  function getState() {
+    return state
+  }
+  return {getState}
+}
+const store = createStore([{id: 1, description: 'Task 1', completed: false}])
 
-  return <h1>{state}</h1>
+const App = () => {
+  console.log(store.getState())
+  return <h1>App</h1>
   
 }
 
