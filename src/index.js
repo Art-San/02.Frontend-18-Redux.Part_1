2465662ca17218202b63ff52e8f7e37a1b3bdf70
Aleksander <asava@sibmail.com>
2023-01-21 19:40:23 +0700

    Разработка Redux с нуля. Reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-function createStore(initialState) {
+function taskReduser(state, action) {
+  switch (action.type) {
+    case 'task/completed':
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
+      newArray[elementIndex].completed = true
+      return newArray
+      
+    default:
+      break;
+  }
+  
+}
+
+function createStore(reducer, initialState) {
   let state = initialState
   
   function getState() {
@@ -9,28 +23,28 @@ function createStore(initialState) {
   }
 
   function dispatch(action) {
-    console.log('action', action)
-    if(action.type === 'task/completed') {
-      const newArray = [...state]
-      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
-      newArray[elementIndex].completed = true
-      state = newArray
-      console.log('state', state)
-    }
+    state = reducer(state, action)
+   
   }
 
   return {getState, dispatch}
 }
-const store = createStore([{id: 1, description: 'Task 1', completed: false}])
+
+const store = createStore(taskReduser, [{id: 1, description: 'Task 1', completed: false}])
 
 const App = () => {
   console.log('store.getState(', store.getState())
-  
-
+  const completeTask = () => {
+    store.dispatch({
+      type: 'task/completed',
+      payload: { id: 1}
+    })
+    console.log(store.getState())
+  }
   return (
     <>
     <h1>App</h1>
-    <button onClick={() => store.dispatch({ type: 'task/completed', payload: { id: 1}})}>Completed</button>
+    <button onClick={completeTask}>Completed</button>
     </>
   )
   
