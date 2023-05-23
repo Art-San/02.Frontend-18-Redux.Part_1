57e8618232503fd4a3afa48f9800d4b0340af16d
Aleksander <asava@sibmail.com>
2023-05-23 23:00:00 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+// функции высшего порядка это те функции которые принимают другие функции в качестве аргумента
+// const App = (params) => {
+//     function someFn() {
+//         return function () {
+//             return 'apps'
+//         }
+//     }
+//     function fn(func) {
+//         return func()
+//     }
+//     const fffk = someFn()
+//     console.log('fffk', fffk)
+//     return <h1>{fn(someFn())}</h1>
+// }
 
 const App = () => {
- const arr = [' some', ' new', ' data']
- function formatArray(el) {return el + ' some'}
- 
-  // setTimeout(() => {
-    
-  // }, timeout);
- 
-  return <h1>{arr.map(formatArray)}</h1>
+    const arr = [' some', ' new', ' data']
+    function formatArray(el) {
+        return el + ' some'
+    }
+
+    // setTimeout(() => {
+
+    // }, timeout);
+
+    return <h1>{arr.map(formatArray)}</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
-
-
-// function someFn() {
-//   return function() {
-//     return 'apps'
-//   }
-// }
-// function fn(func) {
-// return func()
-
-// }
