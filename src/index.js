6f421c025c3332e455d37ac9c0ad5ad0c94463dd
Aleksander <asava@sibmail.com>
2023-05-24 22:48:39 +0700

    Неизменяемый объект

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 const App = () => {
-  const [state, setState] = useState('staring')
-  // setState((prevState) => ({...prevState, name: 'anjela'}))
-  const obj1 = { id: 2, name: 'Some', autor: {name: 'some2'} }
-  // const obj2 = { ...obj1} // true одинаковые ссылки obj1.autor === obj2.autor
-  const obj2 = { ...obj1, autor: { ...obj1.autor } } // false теперь разные ссылки obj1.autor === obj2.autor
-  console.log(obj1.autor === obj2.autor)
-  // console.log(obj2.autor.name)
+    const [state, setState] = useState('staring')
+    // setState((prevState) => ({...prevState, name: 'anjela'}))
+    const obj1 = { id: 2, name: 'Some', autor: { name: 'some2' } }
+    // const obj2 = { ...obj1} // true одинаковые ссылки obj1.autor === obj2.autor
+    const obj2 = { ...obj1, autor: { ...obj1.autor } } // false теперь разные ссылки obj1.autor === obj2.autor
+    console.log(obj1.autor === obj2.autor)
+    // console.log(obj2.autor.name)
 
-  return <h1>{state}</h1>
-  
+    return <h1>{state}</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
