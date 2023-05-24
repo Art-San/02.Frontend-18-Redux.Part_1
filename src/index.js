499273cc96b4ae1d022697cbb431ce7a09483efa
Aleksander <asava@sibmail.com>
2023-05-24 19:25:14 +0700

    Композиция функций 3вар, и pipe(водопровод)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { compose, pipe } from 'lodash/fp'
 
-const App = () => {
-  const x = 2
-  // const y = x * 2
-  // const z = y * y
-  const double = (number) => number * 2
-  const square = (number) => number * number
-  const half = (number) => number / 2
-  // const output = square(double(x))
-  const mathCalculate = pipe(double,  square, half)
+//--------------- первая композиция
+// const App = () => {
+//     const x = 2
+//     const double = (number) => number * 2
+//     const square = (number) => number * number
+//     return <h1>{square(double(x))}</h1>
+// }
 
-  return <h1>{mathCalculate(x)}</h1>
-  
+//--------------- вторая композиция
+// const App = () => {
+//     const x = 2
+//     const double = (number) => number * 2
+//     const square = (number) => number * number
+//     const half = (number) => number / 2
+
+//     return <h1>{half(square(double(x)))}</h1>
+// }
+
+const App = () => {
+    const x = 2
+    const double = (number) => number * 2
+    const square = (number) => number * number
+    const half = (number) => number / 2
+
+    // const mathCalculate = compose(half, square, double)
+    const mathCalculate = pipe(double, square, half)
+
+    return <h1>{mathCalculate(x)}</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
