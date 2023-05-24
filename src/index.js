c8ae614d6e5d91c032d9c4681af5af9c194e8baa
Aleksander <asava@sibmail.com>
2023-05-24 20:49:45 +0700

    Каррирование

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { compose, pipe } from 'lodash/fp'
 
 const App = () => {
-  const x = 2
-  // const y = x * 2
-  // const z = y * y
-  const double = (number) => number * 2
-  const square = (number) => number * number
-  const half = (number) => number / 2
-  // const divider = (num1, num2) => num1 / num2 // не работает
+    const x = 2
+    const double = (number) => number * 2
+    const square = (number) => number * number
+    const half = (number) => number / 2
 
-  //Каррирование
-  const divider = (num2) => {
-    return function(num1) {
-     return num1 / num2
+    //Каррирование
+    const divider = (num2) => {
+        return function (num1) {
+            return num1 / num2
+        }
     }
-  }
-  
-  const mathCalculate = pipe(double,  square, half, divider(3))
-  
 
-  return <h1>{mathCalculate(x)}</h1>
-  
+    const mathCalculate = pipe(double, square, half, divider(3))
+
+    return <h1>{mathCalculate(x)}</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
