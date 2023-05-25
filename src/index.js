4be87763e79feeaa1dccb0ac8db07ee5053d50e7
Aleksander <asava@sibmail.com>
2023-05-25 13:09:14 +0700

    теперь мы можем обновлять состояние для наших элем

--- src/index.js
+++ src/index.js
@@ -1,44 +1,53 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function createStore(initialState) {
-  let state = initialState
-  
-  function getState() {
-    return state
-  }
+    let state = initialState
 
-  function dispatch(action) {
-    console.log('action', action)
-    if(action.type === 'task/completed') {
-      const newArray = [...state]
-      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
-      newArray[elementIndex].completed = true
-      state = newArray
-      console.log('state', state)
+    function getState() {
+        return state
     }
-  }
 
-  return {getState, dispatch}
+    function dispatch(action) {
+        console.log('action', action)
+        if (action.type === 'task/completed') {
+            const newArray = [...state]
+            const elementIndex = newArray.findIndex(
+                (el) => el.id === action.payload.id
+            )
+            newArray[elementIndex].completed = true
+            state = newArray
+            console.log('state', state)
+        }
+    }
+
+    return { getState, dispatch }
 }
-const store = createStore([{id: 1, description: 'Task 1', completed: false}])
+const store = createStore([{ id: 1, description: 'Task 1', completed: false }])
 
 const App = () => {
-  console.log('store.getState(', store.getState())
-  
+    // console.log('store.getState(', store.getState())
 
-  return (
-    <>
-    <h1>App</h1>
-    <button onClick={() => store.dispatch({ type: 'task/completed', payload: { id: 1}})}>Completed</button>
-    </>
-  )
-  
+    return (
+        <>
+            <h1>App</h1>
+            <button
+                onClick={() =>
+                    store.dispatch({
+                        type: 'task/completed',
+                        payload: { id: 1 }
+                    })
+                }
+            >
+                Completed
+            </button>
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
