f48f4f88e8d7d0b628db4dff2a81b77770b90780
Aleksander <asava@sibmail.com>
2023-05-25 12:45:39 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function createStore(initialState) {
-  let state = initialState
-  
-  function getState() {
-    return state
-  }
-  return {getState}
+    let state = initialState
+
+    function getState() {
+        return state
+    }
+    return { getState }
 }
-const store = createStore([{id: 1, description: 'Task 1', completed: false}])
+const store = createStore([{ id: 1, description: 'Task 1', completed: false }])
 
 const App = () => {
-  console.log(store.getState())
-  return <h1>App</h1>
-  
+    console.log(store.getState())
+    return <h1>App</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
