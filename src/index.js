c5dc5e0ceb302a9afebaa8e80db7cc24deecfbcd
Aleksander <asava@sibmail.com>
2023-05-26 13:51:16 +0700

    Observer Pattern

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
-function taskReduser(state, action) {
-  switch (action.type) {
-    case 'task/completed':
-      const newArray = [...state]
-      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
-      newArray[elementIndex].completed = true
-      return newArray
-      
-    default:
-      break;
+// function taskReducer(state, action) {
+//   switch (action.type) {
+//     case 'task/completed':
+//       const newArray = [...state]
+//       const elementIndex = newArray.findIndex(
+//         (el) => el.id === action.payload.id
+//       )
+//       if (newArray[elementIndex].completed === false) {
+//         newArray[elementIndex].completed = true
+//       } else {
+//         newArray[elementIndex].completed = false
+//       }
+
+//       return newArray
+
+//     default:
+//       break
+//   }
+// }
+
+function taskReducer(state, action) {
+  if (action.type === 'task/completed') {
+    const newArray = state.map((el) =>
+      el.id === action.payload.id ? { ...el, completed: !el.completed } : el
+    )
+    return newArray
   }
-  
+  return state
 }
 
 function createStore(reducer, initialState) {
   let state = initialState
   let listeners = []
-  
+
   function getState() {
     return state
   }
@@ -28,17 +44,17 @@ function createStore(reducer, initialState) {
       const listener = listeners[i]
       listener()
     }
-   
   }
   function subscribe(listener) {
+    // Observer Pattern
     listeners.push(listener)
   }
-  return {getState, dispatch, subscribe}
+  return { getState, dispatch, subscribe }
 }
 
-const store = createStore(taskReduser, [
-  {id: 1, description: 'Task 1', completed: false},
-  {id: 2, description: 'Task 2', completed: false}
+const store = createStore(taskReducer, [
+  { id: 1, description: 'Task 1', completed: false },
+  { id: 2, description: 'Task 2', completed: false }
 ])
 
 const App = () => {
@@ -53,26 +69,25 @@ const App = () => {
   const completeTask = (taskId) => {
     store.dispatch({
       type: 'task/completed',
-      payload: { id: taskId}
+      payload: { id: taskId }
     })
   }
 
   return (
     <>
-    <h1>App</h1>
-    <ul>
-      {state.map((el) => (
-        <li key={el.id}>
-          <p>{el.description}</p>
-          <p> {`Comleted: ${el.completed}`}</p>
-          <button onClick={() => completeTask(el.id)}>Completed</button>
-          <hr/>
-        </li>
-      ))}
-    </ul>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.description}</p>
+            <p> {`Comleted: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Completed</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
     </>
   )
-  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
