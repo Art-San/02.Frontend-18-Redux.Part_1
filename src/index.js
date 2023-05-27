0e357fe42f3c77978a3188e1ffc4316f6616284e
Aleksander <asava@sibmail.com>
2023-05-27 18:20:54 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 // import { createStore } from './store/createStore';
 // import { taskReduser } from './store/taskReducer';
 import * as actions from './store/actionTypes'
-import { initiateStore } from './store/store';
+import { initiateStore } from './store/store'
 
 // const initialState = [
 //   {id: 1, title: 'Task 1', completed: false},
@@ -22,37 +22,50 @@ const App = () => {
     })
   }, [])
 
+  // const completeTask = (taskId) => {
+  //   store.dispatch({
+  //     type: actions.taskUpdated,
+  //     payload: { id: taskId, completed: true}
+  //   })
+  // }
+
+  // const changeTitle = (taskId) => {
+  //   store.dispatch({
+  //     type: actions.taskUpdated,
+  //     payload: { id: taskId, title: `New title for ${taskId}`}
+  //   })
+  // }
+
   const completeTask = (taskId) => {
     store.dispatch({
-      type: actions.taskUpdated,
-      payload: { id: taskId, completed: true}
+      type: actions.taskCompleted,
+      payload: { id: taskId }
     })
   }
 
   const changeTitle = (taskId) => {
     store.dispatch({
       type: actions.taskUpdated,
-      payload: { id: taskId, title: `New title for ${taskId}`}
+      payload: { id: taskId }
     })
   }
 
   return (
     <>
-    <h1>App</h1>
-    <ul>
-      {state.map((el) => (
-        <li key={el.id}>
-          <p>{el.title}</p>
-          <p> {`Comleted: ${el.completed}`}</p>
-          <button onClick={() => completeTask(el.id)}>Completed</button>
-          <button onClick={() => changeTitle(el.id)}>Chsange Title</button>
-          <hr/>
-        </li>
-      ))}
-    </ul>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p> {`Comleted: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Completed</button>
+            <button onClick={() => changeTitle(el.id)}>Chsange Title</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
     </>
   )
-  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
--- src/store/taskReducer.js
+++ src/store/taskReducer.js
@@ -1,18 +1,78 @@
-import { taskUpdated } from "./actionTypes";
+import { taskUpdated, taskCompleted } from './actionTypes'
 
-export function taskReduser(state = [], action) {
-    switch (action.type) {
-        case taskUpdated: {
-          const newArray = [...state]
-        const elementIndex = newArray.findIndex(
-          (el) => el.id === action.payload.id
-          )
-          newArray[elementIndex] = {...newArray[elementIndex], ...action.payload}
-          return newArray
-        }
-        
-      default:
-        return state
+// export function taskReducer(state = [], action) {
+//   switch (action.type) {
+//     case taskUpdated: {
+//       const newArray = [...state]
+//       const elementIndex = newArray.findIndex(
+//         (el) => el.id === action.payload.id
+//       )
+//       newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload }
+//       return newArray
+//     }
+
+//     default:
+//       // break // был break хотя должен быть return state так написано в документацци reducer
+//       return state
+//   }
+// }
+
+// export function taskReducer(state = [], action) {
+//   const newArray = [...state]
+//   console.log('action', action.payload)
+//   const elementIndex = newArray.findIndex((el) => el.id === action.payload.id)
+
+//   switch (action.type) {
+//     case taskCompleted:
+//       newArray[elementIndex].completed = !newArray[elementIndex].completed
+//       break
+//     case taskUpdated:
+//       newArray[elementIndex].title =
+//         newArray[elementIndex].title === `Task ${action.payload.id}`
+//           ? `New title for ${action.payload.id}`
+//           : `Task ${action.payload.id}`
+//       break
+//     default:
+//       return state
+//   }
+
+//   return newArray
+// }
+
+export function taskReducer(state = [], action) {
+  console.log(action)
+  switch (action.type) {
+    case taskCompleted:
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      )
+      if (newArray[elementIndex].completed === false) {
+        newArray[elementIndex].completed = true
+      } else {
+        newArray[elementIndex].completed = false
+      }
+      return newArray
+    case taskUpdated: {
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      )
+      if (newArray[elementIndex].title === `Task ${action.payload.id}`) {
+        newArray[elementIndex].title = `New title for ${action.payload.id}`
+      } else {
+        newArray[elementIndex].title = `Task ${action.payload.id}`
+      }
+      // console.log('state', state)
+      // const newArray = state.map((el) =>
+      //   el.id === action.payload.id
+      //     ? { ...el, title: `New title for ${action.payload.id}` }
+      //     : el
+      // )
+      return newArray
     }
-    
+
+    default:
+      return state
   }
+}
