812186fc0ed7e3734671cc35c085ab785a89c82b
Aleksander <asava@sibmail.com>
2023-05-27 19:49:45 +0700

    работает туда сюда

--- src/store/taskReducer.js
+++ src/store/taskReducer.js
@@ -17,62 +17,27 @@ import { taskUpdated, taskCompleted } from './actionTypes'
 //   }
 // }
 
-// export function taskReducer(state = [], action) {
-//   const newArray = [...state]
-//   console.log('action', action.payload)
-//   const elementIndex = newArray.findIndex((el) => el.id === action.payload.id)
-
-//   switch (action.type) {
-//     case taskCompleted:
-//       newArray[elementIndex].completed = !newArray[elementIndex].completed
-//       break
-//     case taskUpdated:
-//       newArray[elementIndex].title =
-//         newArray[elementIndex].title === `Task ${action.payload.id}`
-//           ? `New title for ${action.payload.id}`
-//           : `Task ${action.payload.id}`
-//       break
-//     default:
-//       return state
-//   }
-
-//   return newArray
-// }
-
 export function taskReducer(state = [], action) {
-  console.log(action)
-  switch (action.type) {
-    case taskCompleted:
-      const newArray = [...state]
-      const elementIndex = newArray.findIndex(
-        (el) => el.id === action.payload.id
-      )
-      if (newArray[elementIndex].completed === false) {
-        newArray[elementIndex].completed = true
-      } else {
-        newArray[elementIndex].completed = false
-      }
-      return newArray
-    case taskUpdated: {
-      const newArray = [...state]
-      const elementIndex = newArray.findIndex(
-        (el) => el.id === action.payload.id
-      )
-      if (newArray[elementIndex].title === `Task ${action.payload.id}`) {
-        newArray[elementIndex].title = `New title for ${action.payload.id}`
-      } else {
-        newArray[elementIndex].title = `Task ${action.payload.id}`
-      }
-      // console.log('state', state)
-      // const newArray = state.map((el) =>
-      //   el.id === action.payload.id
-      //     ? { ...el, title: `New title for ${action.payload.id}` }
-      //     : el
-      // )
-      return newArray
+  if (action.payload) {
+    const newArray = [...state]
+    const elementIndex = newArray.findIndex((el) => el.id === action.payload.id)
+
+    switch (action.type) {
+      case taskCompleted:
+        newArray[elementIndex].completed = !newArray[elementIndex].completed
+        break
+      case taskUpdated:
+        newArray[elementIndex].title =
+          newArray[elementIndex].title === `Task ${action.payload.id}`
+            ? `New title for ${action.payload.id}`
+            : `Task ${action.payload.id}`
+        break
+      default:
+        break
     }
 
-    default:
-      return state
+    return newArray
   }
+
+  return state
 }
