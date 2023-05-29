98b23aa4141ff4616986c5a8069d0c674d4f885e
Aleksander <asava@sibmail.com>
2023-05-29 13:12:02 +0700

    исправил чтоб туда сюда

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,15 +1,31 @@
 import * as actionsTypes from './actionTypes'
 
 export function taskComplete(id) {
-    return {
-        type: actionsTypes.taskUpdated,
-        payload: { id, completed: true}
-    }
+  return {
+    type: actionsTypes.taskCompleted,
+    payload: { id }
+  }
 }
 
 export function titleChenge(id) {
-    return {
-        type: actionsTypes.taskUpdated,
-        payload: { id, title: `New title for ${id}`}
-    }
+  return {
+    type: actionsTypes.taskUpdated,
+    payload: { id }
+  }
 }
+
+// import * as actionsTypes from './actionTypes'
+
+// export function taskComplete(id) {
+//     return {
+//         type: actionsTypes.taskUpdated,
+//         payload: { id, completed: true}
+//     }
+// }
+
+// export function titleChenge(id) {
+//     return {
+//         type: actionsTypes.taskUpdated,
+//         payload: { id, title: `New title for ${id}`}
+//     }
+// }
--- src/store/taskReducer.js
+++ src/store/taskReducer.js
@@ -1,18 +1,45 @@
-import { taskUpdated } from "./actionTypes";
+import { taskUpdated, taskCompleted } from './actionTypes'
 
-export function taskReduser(state = [], action) {
+export function taskReducer(state = [], action) {
+  if (action.payload) {
+    const newArray = [...state]
+    const elementIndex = newArray.findIndex((el) => el.id === action.payload.id)
+
     switch (action.type) {
-        case taskUpdated: {
-          const newArray = [...state]
-        const elementIndex = newArray.findIndex(
-          (el) => el.id === action.payload.id
-          )
-          newArray[elementIndex] = {...newArray[elementIndex], ...action.payload}
-          return newArray
-        }
-        
+      case taskCompleted:
+        newArray[elementIndex].completed = !newArray[elementIndex].completed
+        break
+      case taskUpdated:
+        newArray[elementIndex].title =
+          newArray[elementIndex].title === `Task ${action.payload.id}`
+            ? `New title for ${action.payload.id}`
+            : `Task ${action.payload.id}`
+        break
       default:
-        return state
+        break
     }
-    
+
+    return newArray
   }
+
+  return state
+}
+
+// import { taskUpdated } from "./actionTypes";
+
+// export function taskReduser(state = [], action) {
+//     switch (action.type) {
+//         case taskUpdated: {
+//           const newArray = [...state]
+//         const elementIndex = newArray.findIndex(
+//           (el) => el.id === action.payload.id
+//           )
+//           newArray[elementIndex] = {...newArray[elementIndex], ...action.payload}
+//           return newArray
+//         }
+
+//       default:
+//         return state
+//     }
+
+//   }
